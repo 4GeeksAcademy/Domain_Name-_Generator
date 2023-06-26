7bab507be78b4570976ba73235021ef28160cb4c
Mariana <101410421+Placito@users.noreply.github.com>
2023-06-26 12:17:01 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,31 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
+// How to place content in the browser window
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#domain").innerHTML = domainNameGenerator();
 };
+
+//Create a function to generate Domain Names
+function domainNameGenerator() {
+  //the variables with the various arguments that can be used
+  let pronouns = ["the", "our"];
+  let adjs = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let extentions = [".com", ".pt"];
+
+  //the variable that will store the Domain name
+  let domainNames = [];
+
+  //do nested for loops to give all possible name hypotheses
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        for (let extention of extentions) {
+          domainNames.push(pronoun + adj + noun + extention);
+        }
+      }
+    }
+  }
+  return `<ul>${domainNames.map(domainName => `<li>${domainName}</li>`)}</ul>`;
+}
